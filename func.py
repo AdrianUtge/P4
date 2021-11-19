43fcbafb495594ad6e4b04d899ec3b3c3d1d1769
Adrian Utge <adrianutge@gmail.com>
2021-11-19 21:28:51 +0100

    check diag added to function

--- func.py
+++ func.py
@@ -3,6 +3,16 @@ def play(player, column, matrix):
     col = [row[column] for row in matrix]
     if (0 not in col):
         return matrix, False
-    index = 5 - col[::-1].index(0) 
+    index = 5 - col[::-1].index(0)
     matrix[index][column] = player
     return matrix, True
+
+
+def CheckDiag(col):
+    print("Checking Diagonal", col)
+    for j in range(6):
+        if (col[1][j] == 1 and col[0][j + 1] == 1):
+            print("Diagonal Detected")
+        if (col[0][j] == 1 and col[1][j + 1] == 1):
+            print("Diagonal Detected")
+    print("Diagonal Check Complete")
