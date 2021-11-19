050d8c9119b72a568f0ca6afaa03b74ae2d57960
Efili0n <doubleduck300010@gmail.com>
2021-11-19 21:24:50 +0100

    Fixed problem with function play in func.py

--- func.py
+++ func.py
@@ -1,6 +1,8 @@
 def play(player, column, matrix):
     #manque les test (assert) pour les arguments
     col = [row[column] for row in matrix]
-    index = 5 - col[::-1].index(0) #faire un return au cas où il n'y a plus de place dans la colonne
+    if (0 not in col):
+        return matrix, False
+    index = 5 - col[::-1].index(0) 
     matrix[index][column] = player
-    return matrix
+    return matrix, True
