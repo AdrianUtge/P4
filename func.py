68c48684fcb911abb5c9cb6cd62d3de687b1ad5f
Efili0n <doubleduck300010@gmail.com>
2021-11-22 17:11:40 +0100

    New Function fourInRow in func.py

--- func.py
+++ func.py
@@ -1,5 +1,10 @@
 def play(player, column, matrix):
     #manque les test (assert) pour les arguments
+    assert type(player) is int, "player argument needs to be an integer"
+    assert type(matrix) is list, "matrix argument needs to be a list"
+    assert all(list(map(lambda x: len(x) == len(matrix[0]), matrix))), "every matrix's lists needs to be the same length"
+    assert type(column) is int and 0 <= column < len(matrix[0]), "column argument needs to be an integer (...)"
+
     col = [row[column] for row in matrix]
     if (0 not in col):
         return matrix, False
@@ -7,6 +12,14 @@ def play(player, column, matrix):
     matrix[index][column] = player
     return matrix, True
 
+def fourInRow(liste):
+    a, b = 0, 0
+    for i in liste:
+        if (a != i): a, b = i, 1
+        else: b += bool(a)
+        if (b == 4):
+            return a
+    return 0
 
 def CheckDiag(col):
     print("Checking Diagonal", col)
